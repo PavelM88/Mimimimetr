e634ca69c23c97bba78135cea025a15dc5f12f15
Pavel Mamaev <mamai_88@mail.ru>
2021-04-14 22:05:26 +0300

    Create project

---
+++ src/main/java/ru/test/mimimimetr/demo/VotingCatsApplication.java
@@ -0,0 +1,13 @@
+package ru.test.mimimimetr.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingCatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingCatsApplication.class, args);
+    }
+
+}
